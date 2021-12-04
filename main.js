5797a04581a6e55bca869c9de757e4adf8ffe4db
Matthew Jigalin <mattjigs@gmail.com>
2021-12-04 22:05:17 +1100

    Update

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
-const ul = document.querySelector(".items");
+const myForm = document.querySelector("#my-form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const msg = document.querySelector(".msg");
+const userList = document.querySelector("#users");
 
-ul.firstElementChild.textContent = "Hello";
+myForm.addEventListener("submit", onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  if (nameInput.value === "" || emailInput.value === "") {
+    msg.classList.add("error");
+    msg.innerHTML = "Error: please enter a name and email.";
+
+    setTimeout(() => msg.remove(), 3000);
+  } else {
+    const li = document.createElement("li");
+    li.appendChild(
+      document.createTextNode(`${nameInput.value} : ${emailInput.value}`)
+    );
+
+    userList.appendChild(li);
+
+    // Clear Fields
+    nameInput.value = "";
+    emailInput.value = "";
+  }
+}
